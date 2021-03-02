6f30311e959c1120c08690c47c2a29a1bfdff14f
Prashant Gautam <prashantgautam3333@gmail.com>
2021-03-02 13:16:53 +0530

    Setting up environment

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <HeaderContainer>
+            <h1>Test</h1>
+            
+        </HeaderContainer>
+    )
+}
+
+export default Header;
+
+const HeaderContainer =  styled.div`
+color:"red";
+`;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAyiOoCU4QASkD2hijDKwOIHcXQePl4CTw",
+  authDomain: "slack-clone-png.firebaseapp.com",
+  projectId: "slack-clone-png",
+  storageBucket: "slack-clone-png.appspot.com",
+  messagingSenderId: "694835041601",
+  appId: "1:694835041601:web:5af957dd38fcc18f4cbdc0"
+};
